6ae3c0ae9e8a16a063a40164f67a3f74549b3425
Romel Santoral <romel@old.st>
2024-05-29 15:14:43 +0800

    Add changes to the main app file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,174 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./index.css";
+import NumberButton from "./components/NumberButton";
+import OperatorButton from "./components/OperatorButton";
 
-function App() {
+export default function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [result, setResult] = useState(0);
+  const [firstOperand, setFirstOperand] = useState(0);
+  const [operation, setOperation] = useState("");
+
+  const handleDarkmodeCheckboxChange = (event) => {
+    setIsDarkMode(event.target.checked);
+  };
+
+  const handleNumberClick = (number) => {
+    if (number !== "." || !result.includes(".")) {
+      if (result == 0) {
+        setResult(`${number}`);
+      } else {
+        setResult(`${result}${number}`);
+      }
+    }
+  };
+
+  const handleOperatorClick = (op) => {
+    setFirstOperand(result);
+    setOperation(op);
+    setResult(0);
+  };
+
+  const handleEqualButtonClick = () => {
+    let newResult = 0;
+    let firstOp = Number(firstOperand);
+    let secondOp = Number(result);
+    if (operation == "+") {
+      newResult = firstOp + secondOp;
+      setResult(newResult);
+    } else if (operation == "-") {
+      newResult = firstOp - secondOp;
+      setResult(newResult);
+    } else if (operation == "x") {
+      newResult = firstOp * secondOp;
+      setResult(newResult);
+    } else if (operation == "/") {
+      newResult = firstOp / secondOp;
+      setResult(newResult);
+    }
+  };
+
+  const handleClearData = () => {
+    setFirstOperand(0);
+    setOperation("");
+    setResult(0);
+  };
+
+  useEffect(() => {
+    console.log("Changed:", firstOperand, operation, result);
+  }, [firstOperand, operation, result]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <label className="switch">
+        <input
+          type="checkbox"
+          checked={isDarkMode}
+          onChange={(event) => handleDarkmodeCheckboxChange(event)}
+        />
+        <span className="slider round"></span>
+      </label>
+      <div className="calculator">
+        <div
+          className={`calculator__output ${
+            isDarkMode ? "calculator__output__dark" : ""
+          }`}
         >
-          Learn React
-        </a>
-      </header>
+          {result}
+        </div>
+        <div
+          className={`calculator__keys ${
+            isDarkMode ? "calculator__keys__dark" : ""
+          }`}
+        >
+          <OperatorButton
+            label="+"
+            onClick={() => handleOperatorClick("+")}
+            isDarkMode={isDarkMode}
+          />
+          <OperatorButton
+            label="-"
+            onClick={() => handleOperatorClick("-")}
+            isDarkMode={isDarkMode}
+          />
+          <OperatorButton
+            label="&times;"
+            onClick={() => handleOperatorClick("x")}
+            isDarkMode={isDarkMode}
+          />
+          <OperatorButton
+            label="÷"
+            onClick={() => handleOperatorClick("/")}
+            isDarkMode={isDarkMode}
+          />
+          <NumberButton
+            label="7"
+            onClick={() => handleNumberClick(7)}
+            isDarkMode={isDarkMode}
+          />
+          <NumberButton
+            label="8"
+            onClick={() => handleNumberClick(8)}
+            isDarkMode={isDarkMode}
+          />
+          <NumberButton
+            label="9"
+            onClick={() => handleNumberClick(9)}
+            isDarkMode={isDarkMode}
+          />
+          <NumberButton
+            label="4"
+            onClick={() => handleNumberClick(4)}
+            isDarkMode={isDarkMode}
+          />
+          <NumberButton
+            label="5"
+            onClick={() => handleNumberClick(5)}
+            isDarkMode={isDarkMode}
+          />
+          <NumberButton
+            label="6"
+            onClick={() => handleNumberClick(6)}
+            isDarkMode={isDarkMode}
+          />
+          <NumberButton
+            label="1"
+            onClick={() => handleNumberClick(1)}
+            isDarkMode={isDarkMode}
+          />
+          <NumberButton
+            label="2"
+            onClick={() => handleNumberClick(2)}
+            isDarkMode={isDarkMode}
+          />
+          <NumberButton
+            label="3"
+            onClick={() => handleNumberClick(3)}
+            isDarkMode={isDarkMode}
+          />
+          <NumberButton
+            label="0"
+            onClick={() => handleNumberClick(0)}
+            isDarkMode={isDarkMode}
+          />
+          <NumberButton
+            label="."
+            onClick={() => handleNumberClick(".")}
+            isDarkMode={isDarkMode}
+          />
+          <NumberButton
+            label="AC"
+            onClick={() => handleClearData()}
+            isDarkMode={isDarkMode}
+          />
+          <button
+            className="calculator__key calculator__key--enter"
+            onClick={() => handleEqualButtonClick()}
+          >
+            =
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
