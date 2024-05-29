4659313a05439f052baf243c6968d8b61a0c2426
Romel Santoral <romel@old.st>
2024-05-29 15:14:29 +0800

    Create number button component

---
+++ src/components/NumberButton.js
@@ -0,0 +1,12 @@
+export default function NumberButton({label, onClick, isDarkMode}) {
+  return (
+    <button
+      className={`calculator__key ${
+        isDarkMode ? "calculator__key__dark" : ""
+      }`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  )
+}
